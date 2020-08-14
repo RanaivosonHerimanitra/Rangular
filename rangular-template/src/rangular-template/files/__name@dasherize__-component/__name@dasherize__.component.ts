7a2cc7608cc50311846f10f8deec92b50b5a4745
Ranaivoson <air_manitra@yahoo.fr>
2020-08-14 16:38:43 -0400

    building-block-of-methods-transfer

---
+++ rangular-template/src/rangular-template/files/__name@dasherize__-component/__name@dasherize__.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dn-<%=dasherize(name)%>',
+  templateUrl: './<%=dasherize(name)%>.component.html',
+  styleUrls: ['./<%=dasherize(name)%>.component.css'],
+})
+export class <%= classify(name) %>Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  <%=split(methods)%>
+}
--- rangular-template/src/rangular-template/index.ts
+++ rangular-template/src/rangular-template/index.ts
@@ -1,12 +1,32 @@
-import { Rule, SchematicContext, Tree} from '@angular-devkit/schematics';
-
+import { Schema } from './schema.d';
+import { Rule, SchematicContext, Tree, url, apply, template, mergeWith} from '@angular-devkit/schematics';
+import { strings } from '@angular-devkit/core';
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function rangularTemplate(_options: any): Rule {
+export function rangularTemplate(_options: Schema): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    tree.create('rangular.ts', `public give(a: string) {
-      return a + 'b;
-    }`);
-    return tree;
+    const srcComponentTemplate = url('./files');
+    const split = (element:string) => { 
+    const arr = element.split(";").map (value => {
+        return `${value}(){
+          return 'ok';
+        }`
+      })
+    return arr.toString().replace(",","\n\n")
+    }
+    const srcRulesApplication = apply(srcComponentTemplate, [
+    template({
+      ..._options,
+      ...strings,
+      ...{split}
+    })
+  ]);
+  /*tree.create('rangular.ts', `public ${_options.name}(a: string) {
+    return a + 'b;
+  }`);
+  */
+  tree = mergeWith(srcRulesApplication)(tree, _context) as Tree;
+  
+  return tree;
   };
 }
